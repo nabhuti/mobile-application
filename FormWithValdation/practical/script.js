5ea12b281abe07897c0422d5e727f00c552b5916
masande notununu <nmasande25@gmail.com>
2018-11-13 10:58:49 +0200

    tuesday

---
+++ FormWithValdation/practical/script.js
@@ -0,0 +1,55 @@
+
+var app = new Vue({
+    el: '#app',
+    data:{
+        username:'',
+        password :'',
+        usernameError:false,
+        passwordError:false,
+        usernameErrorMessage:'',
+        passwordErrorMessage:''
+    },
+    methods: {
+        validate: function () {
+            this.passwordError=false 
+            this.usernameError=false
+            this.usernameErrorMessage=''
+            this.passwordErrorMessage=''
+
+            if(this.password.length < 6){
+                this.passwordError=true
+                this.passwordErrorMessage='Password is too short'
+            }
+            
+            if(this.username.length < 3){
+                this.usernameError=true
+                this.usernameErrorMessage='Username is too short'
+            }
+            
+            if(!(this.username.includes('@'))){
+                this.usernameError=true
+                this.usernameErrorMessage='Username must be a valid email address'
+            }
+        }
+    }
+})
+
+window.addEventListener('load', function() {
+
+    var webAuth = new auth0.WebAuth({
+      domain: 'dandan.auth0.com',
+      clientID: 'J9_G5vbK3js_sLQEP2zaQTIlS5sfXPTA',
+      responseType: 'token id_token',
+      scope: 'openid',
+      redirectUri: window.location.href
+    });
+  
+    var loginBtn = document.getElementById('btn-login');
+  
+    loginBtn.addEventListener('click', function(e) {
+      e.preventDefault();
+      webAuth.authorize();
+    });
+  
+  });
+
